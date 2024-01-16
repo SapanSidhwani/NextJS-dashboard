f4ab85dae491327745510d2a85143cbb6223e3ed
Sapan <sapansidhwani40@gmail.com>
2024-01-16 17:10:43 +0530

    Chap-3 Completed

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+// Primary Font
+export const inter = Inter({ subsets: ['latin'] });
+// Secondary Font 
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+})
