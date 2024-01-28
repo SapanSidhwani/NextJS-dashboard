b09a9251d67b80935f5d7ce2ec489859f4796c49
Sapan <sapansidhwani40@gmail.com>
2024-01-28 20:07:33 +0530

    Chap-12 Mutating Data

---
+++ app/lib/actions.ts
@@ -0,0 +1,75 @@
+
+'use server';
+
+// To handle type validation
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+// Revalidate and redirect
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    // The amount field is specifically set to coerce (change) from a string to a number while also validating its type.
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+ 
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  // convert the amount into cents:
+  const amountInCents = amount * 100;
+  // Format: "YYYY-MM-DD"
+  const date = new Date().toISOString().split('T')[0];
+  
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  
+  /*
+  Since you're updating the data displayed in the invoices route, you want to clear this cache and trigger a new request to the server. 
+  You can do this with the revalidatePath function from Next.js:
+  */
+  revalidatePath('/dashboard/invoices');
+  // Once the database has been updated, the /dashboard/invoices path will be revalidated, and fresh data will be fetched from the server.
+
+  // At this point, you also want to redirect the user back to the /dashboard/invoices page. You can do this with the redirect function from Next.js:
+  redirect('/dashboard/invoices');
+}
+
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  // Calling revalidatePath will trigger a new server request and re-render the table.
+  revalidatePath('/dashboard/invoices');
+}
